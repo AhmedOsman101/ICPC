c581670d9c56ae9a1db95eed6383d1ac5a214ccc
AhmedOsman101 <trollingosman101@outlook.com>
2024-12-05 22:27:14 +0200

    refactor: Improved code readability and maintainability with modern C++ syntax and formatting.

--- level-1/sheet1.cpp
+++ level-1/sheet1.cpp
@@ -1,47 +1,43 @@
+#include <iomanip>
 #include <iostream>
 #include <math.h>
-#include <string.h>
-#include <iomanip>
 
 using namespace std;
 
-void A()
-{
+void A() {
   string name;
   cin >> name;
   cout << "Hello, " << name << endl;
 }
 
-void B()
-{
+void B() {
   const double PI = 3.141592653;
   double R;
   cin >> R;
   cout << fixed << setprecision(9) << pow(R, 2) * PI << endl;
 }
 
-void C()
-{
+void C() {
   long a, b, c, d;
   cin >> a >> b >> c >> d;
   cout << a * b * c * d << endl;
 }
 
-void D()
-{
+void D() {
   string n1, n2;
   cin >> n1 >> n2;
   /*
     stoi method from the string library converts strings to integers,
-    but using n1[index] returns a char data type which is not compatible with stoi,
-    so I converted it to a string using string method which accepts the length of the string and the value
+    but using n1[index] returns a char data type which is not compatible with
+    stoi, so I converted it to a string using string method which accepts the
+    length of the string and the value
   */
-  int sum = stoi(string(1, n1[n1.length() - 1])) + stoi(string(1, n2[n2.length() - 1]));
+  int sum = stoi(string(1, n1[n1.length() - 1])) +
+            stoi(string(1, n2[n2.length() - 1]));
   cout << sum << endl;
 }
 
-void E()
-{
+void E() {
   int k, x;
   cin >> k >> x;
 
@@ -50,8 +46,7 @@ void E()
   cout << response << endl;
 }
 
-void F()
-{
+void F() {
   long long a, b, c, d, minLeft, minRight;
   string minlName, minrName, minimum;
   cin >> a >> b >> c >> d;
@@ -75,28 +70,23 @@ void F()
   cout << minimum << endl;
 }
 
-void G()
-{
+void G() {
   string s;
   int max = 0, count = 0;
   cin >> s;
 
-  for (short i = 0; i < 3; i++)
-  {
-    if (s[i] == 'R')
-    {
+  for (short i = 0; i < 3; i++) {
+    if (s[i] == 'R') {
       count++;
       max = count;
-    }
-    else
+    } else
       count = 0;
   }
 
   cout << max << endl;
 }
 
-void H()
-{
+void H() {
   float V, T, S, D;
   cin >> V >> T >> S >> D;
   float time = D / V;
@@ -104,8 +94,7 @@ void H()
   cout << ((time < T || S < time) ? "Yes" : "No") << endl;
 }
 
-void I()
-{
+void I() {
   long long n, k, half;
   cin >> n >> k;
 
@@ -114,16 +103,14 @@ void I()
   cout << ((k > half) ? (k - half) * 2 : (k * 2) - 1) << endl;
 }
 
-void J()
-{
+void J() {
   int w;
   cin >> w;
 
   cout << ((w % 2 == 0 && w > 2) ? "YES" : "NO") << endl;
 }
 
-void K()
-{
+void K() {
   string n;
   cin >> n;
   int iterations, len = n.length();
@@ -131,12 +118,10 @@ void K()
   iterations = ceil((float)len / 2);
   iterations = len % 2 == 0 ? iterations : iterations - 1;
 
-  for (int i = 0; i < iterations; i++)
-  {
+  for (int i = 0; i < iterations; i++) {
     int j = iterations * 2 - i;
 
-    if (n[i] != n[j])
-    {
+    if (n[i] != n[j]) {
       cout << "No" << endl;
       return;
     }
@@ -145,105 +130,86 @@ void K()
   cout << "Yes" << endl;
 }
 
-void L()
-{
+void L() {
   int A, B;
   cin >> A >> B;
 
-  if (0 < A && B == 0)
-  {
+  if (0 < A && B == 0) {
     cout << "Gold" << endl;
-  }
-  else if (A == 0 && 0 < B)
-  {
+  } else if (A == 0 && 0 < B) {
     cout << "Silver" << endl;
-  }
-  else if (0 < A && 0 < B)
-  {
+  } else if (0 < A && 0 < B) {
     cout << "Alloy" << endl;
   }
 }
 
-void M()
-{
+void M() {
   int d, steps = 0, i = 5;
   cin >> d;
 
-  while (d != 0)
-  {
-    if (0 <= d - i)
-    {
+  while (d != 0) {
+    if (0 <= d - i) {
       d -= i;
       steps++;
-    }
-    else
+    } else
       i--;
   }
 
   cout << steps << endl;
 }
 
-void N()
-{
+void N() {
   int A, B;
   cin >> A >> B;
 
   cout << ((float)A / 100) * B << endl;
 }
 
-void O()
-{
+void O() {
   int M;
   cin >> M;
 
   cout << (M == 0 ? 24 : 48 - M) << endl;
 }
 
-void P()
-{
+void P() {
   double A, B;
   cin >> A >> B;
 
   cout << fixed << setprecision(6) << ((A - B) / 3) + B << endl;
 }
 
-void Q()
-{
+void Q() {
   int t, a, b, c;
 
   cin >> t;
 
-  for (int i = 0; i < t; i++)
-  {
+  for (int i = 0; i < t; i++) {
     cin >> a >> b >> c;
 
     cout << (a + b == c ? '+' : '-') << endl;
   }
 }
 
-void R()
-{
+void R() {
   int t, a, b, c;
 
   cin >> t;
 
-  for (int i = 0; i < t; i++)
-  {
+  for (int i = 0; i < t; i++) {
     cin >> a >> b >> c;
 
     cout << ((a + b == c || a + c == b || b + c == a) ? "YES" : "NO") << endl;
   }
 }
 
-void S()
-{
+void S() {
   int t, x = 0;
   string cmd;
 
   cin >> t;
 
-  for (int i = 0; i < t; i++)
-  {
+  for (int i = 0; i < t; i++) {
     cin >> cmd;
 
     if (cmd[0] == '+')
@@ -259,37 +225,32 @@ void S()
   cout << x << endl;
 }
 
-void T()
-{
+void T() {
   long long N, sum = 0;
   string A;
   cin >> N >> A;
 
-  for (short i = 0; i < N; i++)
-  {
+  for (short i = 0; i < N; i++) {
     sum += stoi(string(1, A[i]));
   }
 
   cout << sum << endl;
 }
 
-void U()
-{
+void U() {
   long long a, b, c, d;
   cin >> a >> b >> c >> d;
 
   cout << (pow(a, b) <= pow(c, d) ? "NO" : "YES") << endl;
 }
 
-void V()
-{
+void V() {
   long long n;
   cin >> n;
   cout << (n * (n + 1)) / 2 << endl;
 }
 
-void W()
-{
+void W() {
   double a, b;
   cin >> a >> b;
 
@@ -298,8 +259,7 @@ void W()
   cout << "round " << a << " / " << b << " = " << round(a / b) << endl;
 }
 
-void X()
-{
+void X() {
   int a, b, c;
 
   cin >> a >> b >> c;
@@ -307,8 +267,7 @@ void X()
   cout << min(min(a, b), c) << ' ' << max(max(a, b), c) << endl;
 }
 
-int main()
-{
+int main() {
   /*
   A();
   B();
